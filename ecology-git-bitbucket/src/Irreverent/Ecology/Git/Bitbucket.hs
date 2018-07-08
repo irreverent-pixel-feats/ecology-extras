0fb4cdf31bb9b881a072910ea7659269297be1d0
Dom De Re <domdere@domdere.com>
2018-07-08 17:54:33 +1000

    ecology-git-bitbucket
    - Basic API implementation and functions used to make it
    - mafia lock file
    - Export Error type

---
+++ ecology-git-bitbucket/src/Irreverent/Ecology/Git/Bitbucket.hs
@@ -0,0 +1,133 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+-------------------------------------------------------------------
+-- |
+-- Module       : Irreverent.Ecology.Git.Bitbucket
+-- Copyright    : (C) 2018 Irreverent Pixel Feats
+-- License      : BSD-style (see the file /LICENSE.md)
+-- Maintainer   : Dom De Re
+--
+-------------------------------------------------------------------
+module Irreverent.Ecology.Git.Bitbucket (
+  -- * Types
+    GitEcologyError(..)
+  -- * Functions
+  , bitbucketAPI
+  , convertBitbRepo
+  , createNewRepoBitbucket
+  , getOrgReposBitbucket
+  , renderGitEcologyError
+  , toNewBitbRepo
+  ) where
+
+import Irreverent.Ecology.Core.Data (
+    EcologyPrivacy(..)
+  , EcologyProjectDescription(..)
+  , GitRepository(..)
+  , NewGitRepository(..)
+  , ecologyProjectNameText
+  )
+
+import Irreverent.Ecology.API.Git (
+    GitPlatformAPI(..)
+  )
+
+import qualified Irreverent.Bitbucket.Core as B (Username, RepoName(..))
+import qualified Irreverent.Bitbucket.Core.Control as B (BitbucketT(..))
+import qualified Irreverent.Bitbucket.Core.Data.Auth as B (Auth(..))
+import qualified Irreverent.Bitbucket.Core.Data.Common as B
+import qualified Irreverent.Bitbucket.Core.Data.Repository as B (Repository(..))
+import qualified Irreverent.Bitbucket.Core.Data.NewRepository as B
+import qualified Irreverent.Bitbucket.Http.Error as B
+import qualified Irreverent.Bitbucket.Http.Repositories.List as B
+import qualified Irreverent.Bitbucket.Http.Repositories.New as B
+
+import Ultra.Control.Monad.Catch (MonadCatch)
+import Ultra.Control.Monad.Trans.Either (EitherT, firstEitherT, mapEitherT)
+import qualified Ultra.Data.Text as T
+
+import qualified Network.Wreq.Session as S
+
+import Preamble
+
+newtype GitEcologyError =
+  BitbucketAPIError B.BitbucketAPIError
+  deriving (Show)
+
+renderGitEcologyError :: GitEcologyError -> T.Text
+renderGitEcologyError (BitbucketAPIError e) = B.renderBitbucketAPIError e
+
+-- |
+-- A default implementation of a bitbucket backed git platform API
+-- It creates the repo, assigning the project key
+-- as the provided function directs.
+-- It does not modify the access privileges of the
+-- created repository
+--
+bitbucketAPI
+  :: (MonadCatch m, MonadIO m)
+  => (NewGitRepository a -> Maybe B.ProjectKey)
+  -> B.Auth
+  -> B.Username
+  -> GitPlatformAPI a m GitEcologyError
+bitbucketAPI projectFunction auth@(B.Basic user token) org = GitPlatformAPI
+  (user <> ":" <> token)
+  (getOrgReposBitbucket auth org)
+  (createNewRepoBitbucket projectFunction auth org)
+
+convertBitbRepo :: B.Repository -> GitRepository
+convertBitbRepo repo =
+  GitRepository
+    (B.getRepoName . B.repoName $ repo)
+    (B.getUri . B.hrefUrl . B.repoCloneSsh $ repo)
+
+getOrgReposBitbucket
+  :: (MonadCatch m, MonadIO m)
+  => B.Auth
+  -> B.Username
+  -> EitherT GitEcologyError m [GitRepository]
+getOrgReposBitbucket auth org =
+  firstEitherT BitbucketAPIError . mapEitherT (flip runReaderT auth . B.runBitbucketT)$ do
+      session <- liftIO S.newSession
+      bitbRepos <- B.listRepositories session org
+      pure $ fmap convertBitbRepo bitbRepos
+
+toNewBitbRepo
+  :: (NewGitRepository a -> Maybe B.ProjectKey)
+  -> NewGitRepository a
+  -> B.NewRepository
+toNewBitbRepo projectFunction =
+  let
+    privacy' :: EcologyPrivacy -> B.Privacy
+    privacy' EcologyPrivate = B.Private
+    privacy' EcologyPublic = B.Public
+
+    forkPolicy' :: EcologyPrivacy -> B.ForkPolicy
+    forkPolicy' EcologyPrivate = B.NoPublicForksPolicy
+    forkPolicy' EcologyPublic = B.ForkAwayPolicy
+  in B.NewRepository
+    <$> B.RepoDescription . ecologyProjectDescriptionText . newRepoDescription
+    <*> pure B.Git
+    <*> projectFunction
+    <*> forkPolicy' . newRepoPrivacy
+    <*> privacy' . newRepoPrivacy
+    <*> B.Language . newRepoLanguage
+    <*> pure B.NoWiki
+    <*> pure B.HasIssues
+
+createNewRepoBitbucket
+  :: (MonadCatch m, MonadIO m)
+  => (NewGitRepository a -> Maybe B.ProjectKey)
+  -> B.Auth
+  -> B.Username
+  -> NewGitRepository a
+  -> EitherT GitEcologyError m GitRepository
+createNewRepoBitbucket projectFunction auth owner newRepo =
+  firstEitherT BitbucketAPIError . mapEitherT (flip runReaderT auth . B.runBitbucketT) $ do
+    session <- liftIO S.newSession
+    convertBitbRepo <$>
+      B.createRepo
+        session
+        owner
+        (B.RepoName . ecologyProjectNameText . newRepoName $ newRepo)
+        (toNewBitbRepo projectFunction newRepo)
